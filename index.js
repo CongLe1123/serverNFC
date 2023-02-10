425cbc810384d2883bc05afc6d7e9b59eb59d083
Neppun <lcong16012006@gmail.com>
2023-02-10 14:30:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const functions = require('./functions');
+const dict = require('./dict.json');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/get/:set/:imgs', async(req, res)=>{
+    try {
+        const set = req.params.set;
+        let imgs = functions.decrypt(req.params.imgs);
+        imgs = set=='true' ? imgs.split('-')[0] : imgs.split('-');
+
+        let response = [];
+        if (set=='true'){
+
+            const length = dict[imgs];
+            for (let i=1; i<=length; i ++){
+                response.push(`${imgs}${i}`);
+            }
+            
+        } else {
+            response = imgs
+        }
+
+        res.send(response);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+app.listen(5000, () => {
+    console.log('Listening');
+})
